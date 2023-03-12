635c4c46fa9be259a7ac1995e96bdbe7cb10b27d
novruzov_t <novruzov_t@STHQ0129-01>
2023-03-12 11:14:09 +0400

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+
+var ipString = "127.0.0.1";
+var port = 27001;
+
+
+// var ipAddress = IPAddress.Parse(ipString);
+   
+// using var client = new TcpClient();
+// client.Connect(ipAddress, port);
+
+
+
+using var client = new TcpClient(ipString, port);
+
+
+var stream = client.GetStream();
+var bw = new BinaryWriter(stream);
+var br = new BinaryReader(stream);
+
+
+while (true)
+{
+    bw.Write(Console.ReadLine() ?? string.Empty);
+    Console.WriteLine($"Answer: {br.ReadString()}");
+}
---
+++ Server/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+
+var ip = IPAddress.Parse("127.0.0.1");
+var port = 27001; 
+
+var listener = new TcpListener(ip, port);
+
+listener.Start(10);
+
+Console.WriteLine($"{listener.Server.LocalEndPoint} listening...");
+var client = listener.AcceptTcpClient();
+Console.WriteLine($"{client.Client.RemoteEndPoint} connected...");
+
+var stream = client.GetStream();
+var br = new BinaryReader(stream);
+var bw = new BinaryWriter(stream);
+
+
+while (true)
+{
+    Console.WriteLine(br.ReadString());
+    bw.Write("Roger that!");
+}
