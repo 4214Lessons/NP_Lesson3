af6a0590bf4129f7d64051d068896c0c0da704bd
novruzov_t <novruzov_t@STHQ0129-01>
2023-03-12 12:58:48 +0400

    add group chat

---
+++ ChatClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Sockets;
+
+using var client = new TcpClient("10.2.29.93", 27001);
+
+var serverStream = client.GetStream();
+var bw = new BinaryWriter(serverStream);
+var br = new BinaryReader(serverStream);
+
+Task.Run(() =>
+{
+    while (true)
+    {
+        Console.WriteLine(br.ReadString());
+    }
+});
+
+while (true)
+{
+    bw.Write(Console.ReadLine() ?? string.Empty);
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var clients = new HashSet<TcpClient>();
+
+var ip = IPAddress.Parse("10.2.29.93");
+var port = 27001;
+
+
+var listener = new TcpListener(ip, port);
+
+listener.Start(10);
+
+while (true)
+{
+    var client = listener.AcceptTcpClient();
+    Console.WriteLine($"{client.Client.RemoteEndPoint} connected...");
+    clients.Add(client);
+
+    Task.Run(() =>
+    {
+        var clientStream = client.GetStream();
+        var br = new BinaryReader(clientStream);
+
+        var readString = string.Empty;
+        var userIPEndPoint = string.Empty;
+        var index = 0;
+
+        Console.ForegroundColor = (ConsoleColor)(Random.Shared.Next(15) + 1);
+
+        while (true)
+        {
+            readString = br.ReadString();
+            // Console.WriteLine($"{clientStream.Socket.RemoteEndPoint} - {readString}");
+
+            index = readString.IndexOf(' ');
+            userIPEndPoint = readString.Substring(0, index);
+
+            var destination = clients.FirstOrDefault(c => c.Client.RemoteEndPoint?.ToString() == userIPEndPoint);
+
+            var bw = new BinaryWriter(destination!.GetStream());
+            bw.Write($"{clientStream.Socket.RemoteEndPoint} - {readString.Substring(index + 1)}");
+        }
+    });
+}
